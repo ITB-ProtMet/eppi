6cf32a035677c5a1169ff50aa8ff515e7a5ed8c0
Pietro Brunetti <gunzapper@gmail.com>
2015-02-17 11:48:00 +0100

    better make.py. Writen more clearly and with the installer option

--- make.py
+++ make.py
@@ -26,36 +26,78 @@ arg = sys.argv[1] if len(sys.argv) >= 2 else None
 # or if it is not recent
 #-------------------------------------------------
 
-if not os.path.exist(r'./EPPI/peptidome/commons/build') or \
-os.path.getmtime(r'./EPPI/peptidome/commons/build') < os.path.getmtime(r'./EPPI/peptidome/commons/Param.pyx'):
+build_exist = os.path.exists(r'./EPPI/peptidome/commons/build')
+time_build = os.path.getmtime(r'./EPPI/peptidome/commons/build') 
+time_param = os.path.getmtime(r'./EPPI/peptidome/commons/Param.pyx')
+
+if not build_exist or time_build < time_param:
     os.chdir(r"./EPPI/peptidome/commons/")
-    subprocess.call(['python', 'setup.py', "build_ext",'--inplace']) 
+    subprocess.call([
+        'python', 
+        'setup.py', 
+        "build_ext",
+        '--inplace'
+    ]) 
     os.chdir(r'../../../')
    
 if arg == 'dist':
-     if len(sys.argv) == 3:
-         pyinst_path = sys.argv[2]
-     else:
-         pyinst_path = raw_input("Path to pyinstaller: ")
-     clean()
-     subprocess.call(['python', os.path.join(pyinst_path, 'pyinstaller.py'), 'EPPI.spec'])
+    if len(sys.argv) == 3:
+        pyinst_path = sys.argv[2]
+    else:
+        pyinst_path = raw_input("Path to pyinstaller: ")
+    clean()
+    subprocess.call([
+        'python', 
+        os.path.join(pyinst_path, 'pyinstaller.py'), 
+        'EPPI.spec'
+    ])
 
 elif arg == 'deb':
     print "Ensure you have the python-stdeb package installed!"
-    subprocess.call(['python', 'setup.py', '--command-packages=stdeb.command', 'bdist_deb'])
+    subprocess.call([
+        'python', 
+        'setup.py', 
+        '--command-packages=stdeb.command', 
+        'bdist_deb'
+    ])
 
 elif arg == 'rpm':
-    subprocess.call(['python', 'setup.py', 'bdist_rpm', '--post-install=rpm/postinstall', '--pre-unistall=rpm/preuninstall'])
+    subprocess.call([
+        'python', 
+        'setup.py', 
+        'bdist_rpm', 
+        '--post-install=rpm/postinstall', 
+        '--pre-unistall=rpm/preuninstall'
+    ])
 
 elif arg == 'source':
-    subprocess.call(['python', 'setup.py', 'sdist'])
+    subprocess.call([
+        'python', 'setup.py', 'sdist'
+    ])
 
 elif arg == 'run':
-    subprocess.call(['python', os.path.join('EPPI', "EPPI.py")])
+    subprocess.call([
+        'python', os.path.join('EPPI', "EPPI.py")
+    ])
 
 elif arg == 'test':
     subprocess.call(['nosetests', '--with-doc'])
 
 elif arg == 'clean':
     clean()
- 
+
+elif arg == 'inst':
+    if os.platform != 'win32':
+	print "This option is only avaible on ms windows os"
+        sys.exit(0)        
+
+    if not os.path.exists('./dist/EPPI.exe'):
+        print "please, excecute before 'pymake.py dist'"
+        sys.exit(0)
+     
+    if len(sys.argv) == 3:
+        inno_path = sys.argv[2]
+    else:
+        inno_path = raw_input("path to Inno setup ISCC.exe: ")
+        subprocess.call([inno_path, 'scb.iss']) 
+
